c96078b829b9306e9252b64d034db6cf8f610f91
mtickg <bhMTickgYai@proton.me>
2022-12-22 11:37:44 -0600

    color changes

--- lua/onefinn/theme.lua
+++ lua/onefinn/theme.lua
@@ -263,9 +263,9 @@ function theme.highlights(colors, config)
       DashboardFooter = { fg = colors.green, style = "italic" },
 
       -- normal text and background color
-      Normal = { fg = colors.fg, bg = colors.bg },
-      NormalNC = { bg = colors.bg },
-      SignColumn = { fg = colors.fg, bg = colors.none },
+      Normal = { fg = colors.fg, bg = colors.none },
+      NormalNC = { bg = colors.none },
+      SignColumn = { fg = colors.fg, bg = colors.active },
 
       -- the column separating vertically split windows
       VertSplit = { fg = colors.bg },
