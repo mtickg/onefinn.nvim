c5ca29726b09fd89ea7cbf08ab4d4b99e6a5de8d
mtickg <bhMTickgYai@proton.me>
2022-12-28 11:27:19 -0600

    cc

--- lua/onefinn/theme.lua
+++ lua/onefinn/theme.lua
@@ -794,8 +794,8 @@ function theme.highlights(colors, config)
       NeoTreeEndOfBuffer = { fg = colors.float },
       NeoTreeRootName = { fg = colors.cyan, style = "bold" },
       NeoTreeDirectoryIcon = { fg = colors.yellow },
-      NeoTreeFloatBorder = { fg = colors.active, bg = colors.none },
-      NeoTreeFloatTitle = { fg = colors.cyan, bg = colors.active },
+      NeoTreeFloatBorder = { fg = colors.float, bg = colors.none },
+      NeoTreeFloatTitle = { fg = colors.cyan, bg = colors.float, style = "bold" },
     }
 
     -- Options:
